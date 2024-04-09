6c76c6f3790c53c91ed6dbe635a9f79ae40ebbb9
Christopher Cialone <ccialone0929@gmail.com>
2024-04-09 12:52:53 -0400

    Created Loop - where multiple guesses can happen until correct guess is given

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,20 +4,25 @@ fn main() {
     let correct = rand::thread_rng().gen_range(1..=10);
     println!("correct: {correct}");
     println!("Guess the number from 1 to 10!");
-    let mut guess = String::new();
-
     // let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Error with parse");
+        let guess: u32 = guess.trim().parse().expect("Error with parse");
 
-    let message = match guess.cmp(&correct) {
-        Ordering::Greater => "You guessed too high, Cheech!",
-        Ordering::Less => "You have guessed too low, Cretin",
-        Ordering::Equal => "You guessed the Perfectly", 
-    };
-    println!("{message}");
+        let message = match guess.cmp(&correct) {
+            Ordering::Greater => "You guessed too high, Cheech!",
+            Ordering::Less => "You have guessed too low, Cretin",
+            Ordering::Equal => { 
+                println!("You guessed the Perfectly"); 
+                break;
+            },
+        };
+
+        println!("{message}");
+    }
 }
