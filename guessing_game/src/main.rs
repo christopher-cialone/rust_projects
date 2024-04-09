df8531a91d836acd7c1dacd02fcc4780f9db8fb0
Christopher Cialone <ccialone0929@gmail.com>
2024-04-09 10:05:31 -0400

    Conditional Statement

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,13 +4,21 @@ fn main() {
     let correct = rand::thread_rng().gen_range(1..=10);
     println!("correct: {correct}");
     println!("Guess the number from 1 to 10!");
-    let mut num = String::new();
+    let mut guess = String::new();
 
     // let mut guess = String::new();
 
     io::stdin()
-        .read_line(&mut num)
+        .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", num.trim());
+    let guess: u32 = guess.trim().parse().expect("Error with parse");
+
+    if correct == guess {
+        println!("You guessed the correct number!");
+    }  else {
+        println!("You have guessed incorrectly, my good sir")
+    }
+
+   
 }
