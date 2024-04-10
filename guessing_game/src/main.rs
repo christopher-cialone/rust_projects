53050eca4d2275b73250c96ff75f5dc23b5bd352
Christopher Cialone <ccialone0929@gmail.com>
2024-04-10 10:13:35 -0400

    Completed first version

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,28 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 fn main() {
-    let correct = rand::thread_rng().gen_range(1..=10);
-    // println!("correct: {correct}");
-    println!("Guess the number from 1 to 10!");
-    // let mut guess = String::new();
+    let mut how_many = String::new();
+    println!("How many random numbers would you like to guess, Human-Being?");
+    io::stdin()
+        .read_line(&mut how_many)
+        .expect("Failed to read line");
 
-    loop {
+    let num_guesses: u8 = how_many.trim().parse().expect("Error reading input");
+
+    let mut correct = Vec::new();
+
+    for _ in 0..num_guesses {
+        correct.push(rand::thread_rng().gen_range(1..=10));
+    }
+
+    println!("{correct:?}");
+
+    println!("Guess a number from 1 to 10!");
+
+    let mut guesses_made = 0;
+
+    println!("Guess a number from 1 to 10!");
+    while guesses_made < num_guesses {
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -20,13 +36,20 @@ fn main() {
             }
         };
 
-        match guess.cmp(&correct) {
-            Ordering::Greater => println!("You guessed too high, Cheech!"),
-            Ordering::Less => println!("You have guessed too low, Cretin"),
-            Ordering::Equal => { 
-                println!("You guessed the Perfectly"); 
-                break;
-            },
+        match guess.cmp(&correct[guesses_made as usize]) {
+            Ordering::Greater => println!("You guessed too high, no Bitcoin for you!"),
+            Ordering::Less => println!("You have guessed too low, still no Bitcoin"),
+            Ordering::Equal => {
+                println!("You guessed the Perfectly - we are owed a Bitcoin");
+                guesses_made += 1;
+                if guesses_made < num_guesses {
+                    println!("Let's try the next one")
+                }
+            }
         };
+    }
+    println!("Thanks for playing the game - The correct answer were:");
+    for item in correct {
+        println!("{item}")
     }
 }
